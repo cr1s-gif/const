0413a906ed0c1891309ea9c4519d172fde591337
cr1s-gif <cristobal.collado@mail.udp.cl>
2025-07-17 16:56:15 -0400

    tercer commit

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import '/home/cr1s-gif/const/src/index.css';
+
 
 export const Presentation: React.FC = () => {
   return (
-    <section className=" flex w-full py-20 px-4 text-center bg-gradient-to-b from-blue-700 via-blue-700 to-blue-300">
-      <div className='w-1/2 bg-white rounded-l-lg '></div>
-      <div className='w-1/2 h-full flex item-center justify-center bg-white shadow-xl rounded-r-lg'>
-      <h1 className="text-4xl font-bold mb-4 text-right">Welcome to LandingCo</h1>
-      <p className="text-xl mb-8 max-w-2xl mx-auto text-right">
+    <section className=" flex w-full py-20 px-4 flex item-center justify-center bg-gradient-to-b from-blue-700 via-blue-700 to-blue-300">
+      <div className='w-96 bg-white rounded-l-lg shadow-xl/30'></div>
+      <div className='w-96 h-96 display-flex bg-white shadow-xl/30 rounded-r-lg relative'>
+      <p className='text-xl m-8 max-w-2xl mx-auto text-center cherry-font'>
         We help businesses create stunning landing pages that convert visitors into customers.
       </p>
-      <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg text-lg">
+      <button className="absolute bottom-20 bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg text-lg">
         Learn More
       </button>
       </div>
